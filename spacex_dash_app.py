a81bc361f072306dcc44e6ae733e98428db6d11f
David Hwang <hwangdav000@gmail.com>
2023-12-08 02:34:59 -0600

    Add files via upload

---
+++ spacex_dash_app.py
@@ -0,0 +1,105 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Create an app layout
+app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
+                                        style={'textAlign': 'center', 'color': '#503D36',
+                                               'font-size': 40}),
+                                # TASK 1: Add a dropdown list to enable Launch Site selection
+                                # The default select value is for ALL sites
+                                # dcc.Dropdown(id='site-dropdown',...)
+                                html.Div(dcc.Dropdown(id='site-dropdown',
+                                        options=[
+                                            {'label': 'All Sites', 'value': 'ALL'},
+                                            {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+                                            {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
+                                            {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+                                            {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'},
+                                        ],
+                                        value='ALL',
+                                        placeholder="Select a Launch Site here",
+                                        searchable=True
+                                    )),
+                                html.Br(),
+
+                                # TASK 2: Add a pie chart to show the total successful launches count for all sites
+                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
+                                html.Div(dcc.Graph(id='success-pie-chart')),
+                                html.Br(),
+
+                                html.P("Payload range (Kg):"),
+                                # TASK 3: Add a slider to select payload range
+                                #dcc.RangeSlider(id='payload-slider',...)
+                                dcc.RangeSlider(id='payload-slider',
+                                    min=0, max=10000, step=2500,
+                                    marks={0: '0',
+                                        2500: '2500',
+                                        5000: '5000',
+                                        7500: '7500',
+                                        10000: '10000'},
+                                    value=[min_payload, max_payload]),
+                                # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+                                html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+                                ])
+
+# TASK 2:
+# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Function decorator to specify function input and output
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+def get_pie_chart(entered_site):
+    filtered_df = spacex_df[spacex_df['class']==1]
+    data = filtered_df.groupby('Launch Site')['class'].count().reset_index(name='Count')
+    data['Success Rate'] = data['Count'] / len(filtered_df)
+    if entered_site == 'ALL':
+        fig = px.pie(data, values='Success Rate', 
+        names='Launch Site', 
+        title='Total Sucess Launches By Site')
+        return fig
+    else:
+        # return the outcomes piechart for a selected site
+        filtered_df = spacex_df[spacex_df['Launch Site']==entered_site]
+        success_rate = filtered_df['class'].mean()
+
+        fig = px.pie(values=[success_rate, 1-success_rate], 
+        names=['Success', 'Failure'], 
+        title='Total Success Launches for site {entered_site}')
+        return fig
+
+
+# TASK 4:
+# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
+              [Input(component_id='site-dropdown', component_property='value'),Input(component_id='payload-slider', component_property='value')])
+def get_payload_scatter_chart(entered_site, payload):
+#     If ALL sites are selected, render a scatter plot to display all values for variable Payload Mass (kg) and variable class.
+# In addition, the point color needs to be set to the booster version i.e., color="Booster Version Category"
+    print(payload)
+    if entered_site == 'ALL':
+        filtered_df = spacex_df[(spacex_df['Payload Mass (kg)'] >= payload[0]) & (spacex_df['Payload Mass (kg)'] <= payload[1])]
+        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category')
+        return fig
+    else:
+# If a specific launch site is selected, you need to filter the spacex_df first, and render a scatter chart to show
+# values Payload Mass (kg) and class for the selected site, and color-label the point using Boosster Version Category likewise.
+        filtered_df = spacex_df[spacex_df['Launch Site']==entered_site]
+        filtered_df = filtered_df[(filtered_df['Payload Mass (kg)'] >= payload[0]) & (filtered_df['Payload Mass (kg)'] <= payload[1])]
+        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category')
+        return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
